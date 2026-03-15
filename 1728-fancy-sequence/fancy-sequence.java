e07ad9f01bc168ace31763d338ca864dd642455c
Kripalini Gudadhe <kripalinigudadhe3@gmail.com>
2026-03-15 10:02:01 +0530

    Time: 51 ms (51.72%) | Memory: 124.4 MB (75.86%) - LeetSync

---
+++ 1728-fancy-sequence/fancy-sequence.java
@@ -0,0 +1,54 @@
+import java.util.*;
+
+class Fancy {
+
+    long MOD = 1000000007;
+    List<Long> list;
+    long mul;
+    long add;
+
+    public Fancy() {
+        list = new ArrayList<>();
+        mul = 1;
+        add = 0;
+    }
+
+    public void append(int val) {
+        long v = ((val - add) % MOD + MOD) % MOD;
+        v = (v * modInverse(mul)) % MOD;
+        list.add(v);
+    }
+
+    public void addAll(int inc) {
+        add = (add + inc) % MOD;
+    }
+
+    public void multAll(int m) {
+        mul = (mul * m) % MOD;
+        add = (add * m) % MOD;
+    }
+
+    public int getIndex(int idx) {
+        if (idx >= list.size()) return -1;
+
+        long val = list.get(idx);
+        return (int)((val * mul + add) % MOD);
+    }
+
+    private long modInverse(long x) {
+        return power(x, MOD - 2);
+    }
+
+    private long power(long a, long b) {
+        long res = 1;
+        a %= MOD;
+
+        while (b > 0) {
+            if ((b & 1) == 1) res = (res * a) % MOD;
+            a = (a * a) % MOD;
+            b >>= 1;
+        }
+
+        return res;
+    }
+}
