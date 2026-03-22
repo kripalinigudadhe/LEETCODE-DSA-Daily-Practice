7bbd74b6782d0f9617e7aa3f010ce17d44240f70
Kripalini Gudadhe <kripalinigudadhe3@gmail.com>
2026-03-22 16:49:03 +0530

    Time: 3 ms (90.72%) | Memory: 43.6 MB (65.98%) - LeetSync

---
+++ 939-valid-permutations-for-di-sequence/valid-permutations-for-di-sequence.java
@@ -0,0 +1,26 @@
+class Solution {
+    public int numPermsDISequence(String s) {
+        int mod = 1_000_000_007;
+        int n = s.length();
+        int[][] dp = new int[n+1][n+1];
+
+        for (int j = 0; j <= n; j++) dp[0][j] = 1;
+
+        for (int i = 1; i <= n; i++) {
+            if (s.charAt(i-1) == 'I') {
+                int sum = 0;
+                for (int j = 0; j <= n-i; j++) {
+                    sum = (sum + dp[i-1][j]) % mod;
+                    dp[i][j] = sum;
+                }
+            } else {
+                int sum = 0;
+                for (int j = n-i; j >= 0; j--) {
+                    sum = (sum + dp[i-1][j+1]) % mod;
+                    dp[i][j] = sum;
+                }
+            }
+        }
+        return dp[n][0];
+    }
+}
