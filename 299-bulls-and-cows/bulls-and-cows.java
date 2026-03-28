ec4d8542d8479460f86a6fc0d5cf9d5d710c7ce4
Kripalini Gudadhe <kripalinigudadhe3@gmail.com>
2026-03-28 16:46:04 +0530

    Time: 3 ms (95.90%) | Memory: 43.7 MB (55.75%) - LeetSync

---
+++ 299-bulls-and-cows/bulls-and-cows.java
@@ -0,0 +1,19 @@
+class Solution {
+    public String getHint(String secret, String guess) {
+        int bulls = 0, cows = 0;
+        int[] count = new int[10];
+
+        for (int i = 0; i < secret.length(); i++) {
+            char s = secret.charAt(i);
+            char g = guess.charAt(i);
+
+            if (s == g) bulls++;
+            else {
+                if (count[s - '0']-- > 0) cows++;
+                if (count[g - '0']++ < 0) cows++;
+            }
+        }
+
+        return bulls + "A" + cows + "B";
+    }
+}
