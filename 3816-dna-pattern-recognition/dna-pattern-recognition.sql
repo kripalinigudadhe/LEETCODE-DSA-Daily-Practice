e999b0a50c440e92778eb3a490829390cde9375c
Kripalini Gudadhe <kripalinigudadhe3@gmail.com>
2026-04-05 23:46:29 +0530

    Time: 302 ms (22.76%) | Memory: 0B (100.00%) - LeetSync

---
+++ 3816-dna-pattern-recognition/dna-pattern-recognition.sql
@@ -0,0 +1,27 @@
+SELECT 
+    sample_id,
+    dna_sequence,
+    species,
+
+    CASE 
+        WHEN dna_sequence REGEXP '^ATG' THEN 1 
+        ELSE 0 
+    END AS has_start,
+
+    CASE 
+        WHEN dna_sequence REGEXP '(TAA|TAG|TGA)$' THEN 1 
+        ELSE 0 
+    END AS has_stop,
+
+    CASE 
+        WHEN dna_sequence LIKE '%ATAT%' THEN 1 
+        ELSE 0 
+    END AS has_atat,
+
+    CASE 
+        WHEN dna_sequence REGEXP 'G{3,}' THEN 1 
+        ELSE 0 
+    END AS has_ggg
+
+FROM Samples
+ORDER BY sample_id;
