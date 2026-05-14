e48d163bab15194f85844db549c2f9b46118e9bf
Kripalini Gudadhe <kripalinigudadhe3@gmail.com>
2026-05-14 10:24:44 +0530

    Time: 1 ms (91.05%) | Memory: 44.3 MB (95.43%) - LeetSync

---
+++ 2892-check-if-array-is-good/check-if-array-is-good.java
@@ -0,0 +1,26 @@
+class Solution {
+    public boolean isGood(int[] nums) {
+        int n = nums.length - 1;
+
+        int[] freq = new int[n + 1];
+
+        for (int x : nums) {
+            // Number should be between 1 and n
+            if (x < 1 || x > n) {
+                return false;
+            }
+
+            freq[x]++;
+        }
+
+        // Numbers 1 to n-1 should appear exactly once
+        for (int i = 1; i < n; i++) {
+            if (freq[i] != 1) {
+                return false;
+            }
+        }
+
+        // Number n should appear exactly twice
+        return freq[n] == 2;
+    }
+}
