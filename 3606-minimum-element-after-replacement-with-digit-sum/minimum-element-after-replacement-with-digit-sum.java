cdbe3d014d291e111c852c3eaab470e2bb56e420
Kripalini Gudadhe <kripalinigudadhe3@gmail.com>
2026-05-29 14:42:30 +0530

    Time: 1 ms (100.00%) | Memory: 44.9 MB (55.29%) - LeetSync

---
+++ 3606-minimum-element-after-replacement-with-digit-sum/minimum-element-after-replacement-with-digit-sum.java
@@ -0,0 +1,19 @@
+class Solution {
+    public int minElement(int[] nums) {
+        int min = Integer.MAX_VALUE;
+
+        for (int num : nums) {
+            int sum = 0;
+
+            // Calculate digit sum
+            while (num > 0) {
+                sum += num % 10;
+                num /= 10;
+            }
+
+            min = Math.min(min, sum);
+        }
+
+        return min;
+    }
+}
