fe2c686342c25dcd685c110ffd39f5aeef329bbb
Kripalini Gudadhe <kripalinigudadhe3@gmail.com>
2026-09-15 13:32:09 +0530

    Time: 150 ms (16.08%) | Memory: 62.3 MB (16.08%) - LeetSync

---
+++ 2559-maximum-number-of-non-overlapping-palindrome-substrings/maximum-number-of-non-overlapping-palindrome-substrings.java
@@ -0,0 +1,48 @@
+class Solution {
+    public int maxPalindromes(String s, int k) {
+
+        int n = s.length();
+
+        // palindrome[i][j] = true if s[i...j] is palindrome
+        boolean[][] palindrome = new boolean[n][n];
+
+        // Find all palindromes
+        for (int len = 1; len <= n; len++) {
+            for (int i = 0; i + len <= n; i++) {
+
+                int j = i + len - 1;
+
+                if (len == 1) {
+                    palindrome[i][j] = true;
+                }
+                else if (len == 2) {
+                    palindrome[i][j] = (s.charAt(i) == s.charAt(j));
+                }
+                else {
+                    palindrome[i][j] =
+                        s.charAt(i) == s.charAt(j)
+                        && palindrome[i + 1][j - 1];
+                }
+            }
+        }
+
+        // dp[i] = maximum palindromes using first i characters
+        int[] dp = new int[n + 1];
+
+        for (int i = 1; i <= n; i++) {
+
+            // Don't take a palindrome ending at i-1
+            dp[i] = dp[i - 1];
+
+            // Try every possible starting position
+            for (int j = 0; j <= i - k; j++) {
+
+                if (palindrome[j][i - 1]) {
+                    dp[i] = Math.max(dp[i], dp[j] + 1);
+                }
+            }
+        }
+
+        return dp[n];
+    }
+}
